897430dad10c0cc4a8b1988287e772ad1d0bf1cf
Luis Miguel Lopez <luis.lopezl@sofka.com.co>
2024-06-20 10:39:13 -0500

    Add logic for Bands

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,7 +1,18 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Metallica'));
+bands.addBand(new Band('John Bobi'));
+bands.addBand(new Band('Metal Mania'));
+bands.addBand(new Band('Killing Park'));
 
 // Mensajes de Sockets
 io.on('connection', client => {
+    client.emit('active-bands', bands.getBands());
+
     console.log('Cliente conectado');
 
     client.on('disconnect', () => {
@@ -10,7 +21,26 @@ io.on('connection', client => {
 
     client.on('mensaje', (payload) => {
         console.log('Mensaje!', payload);
-
         io.emit('mensaje', { admin: 'Nuevo mensaje' });
+    });
+
+    client.on('emitir-mensaje', (payload) => {
+        // io.emit('nuevo-mensaje', payload); // Emite a todos
+        client.broadcast.emit('nuevo-mensaje', payload); // Emite a todos menos al que lo emitió
+    });
+
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('add-band', (payload) => {
+        bands.addBand(new Band(payload.name));
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
     });
 });
